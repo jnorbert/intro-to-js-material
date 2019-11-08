078b918d92db56ee13c5a0d8d58ad632e8b0f46b
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2019-11-08 11:42:22 +0000

    Added in activity 1 code for lesson 10

---
+++ lesson_10/activity_1.js
@@ -0,0 +1,54 @@
+// Introduction to JavaScript by Mark Winteringham
+// Lesson 10 - Activity 1 - Loops
+
+// Below are a series of incomplete loops. Complete them so that they output
+// the expected data stated in the loop challenge.
+
+// Challenge 1 - Incrementing loop
+// Currently this loop doesn't run at all. Update the loop so that it counts up
+// from 0 all the way to 10:
+
+for(i = 1; i < 0; i){
+    console.log('Challenge 1 count: ' + i);
+}
+
+// Challenge 2 - Decreasing loop
+// Currently this loop doesn't run at all. Update the loop so that it counts down
+// from 10 down to 1:
+
+for(i = 1; i == 0; i){
+    console.log('Challenge 2 count: ' + i);
+}
+
+// Challenge 3 - Build a for loop from scratch
+// Create a for loop that starts at five and counts up to 15:
+
+console.log('Challenge 3 - Incomplete');
+
+// Challenge 4 - Loop through staff names
+// Create a for loop that loops through each of the staff names in the array
+// below. TIP: You will need to research how to get the numerical size of the
+// array. 
+
+let staff = ['Bernard', 'Manny', 'Fran'];
+
+for(i = 1; i < 0; i){
+    console.log('Challenge 2 count: ' + i);
+}
+
+// Challenge 5 - Loop through staff names again
+// Create a loop that loops through each of the staff names. This time
+// using a for / in loop instead of a standard for loop (Feel free to Google it)
+
+console.log('Challenge 5 - Incomplete');
+
+// Challenge 6 - Incrementing while loop
+// Using a while loop and an if statement
+
+console.log('Challenge 6 - Incomplete');
+
+// Challenge 7 - Break a loop
+// Using a for loop, an if statement and the keyword break (Google it). Have a loop
+// count up to to 10 but break the loop early when the count reaches 5
+
+console.log('Challenge 7 - Incomplete');
